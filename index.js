ae279142b632755ac2698cc5c17ce293d6de1266
Victoriia88 <vulynets528@gmail.com>
2023-07-19 15:24:27 +0300

    create HomeWork10

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function removeElement(array, item) {
-  const index = array.indexOf(item);
-  if (index !== -1) {
-    array.splice(index, 1);
-  }
+let lengtsOfArray = parseInt(prompt("Введіть, будь ласка, довжину масиву?"));
+let array = [];
+
+for (i = 0; i < lengtsOfArray; i++) {
+  let elementArray = prompt("Введіть елемент масиву:");
+  array.push(elementArray);
 }
+console.log(`Масив до сортування: ${array}`);
+
+array.sort();
+console.log(`Відсортований масив: ${array}`);
+
+array.splice(1, 3);
+console.log(`Масив з видаленими елементами: ${array}`);
