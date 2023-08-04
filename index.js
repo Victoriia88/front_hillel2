a91f8fa562a1df93f94192d2c2f137ae4507bfd1
Victoriia88 <vulynets528@gmail.com>
2023-08-04 21:30:59 +0300

    create hw18

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-function createSumFunction() {
-  let currentSum = 0;
+function findDates(input) {
+  const dateRegex = /\b(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\b/g;
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-  return function sum(value) {
-    currentSum += value;
-    return currentSum;
-  };
+  const matches = input.match(dateRegex);
+  if (!matches) return [];
+
+  const result = matches.map((match) => {
+    const [year, month, day] = match.split("-").map(Number);
+    return {
+      day,
+      month,
+      monthName: months[month - 1],
+      year,
+    };
+  });
+
+  return result;
 }
 
-const sum = createSumFunction();
-
-console.log(sum(3));
-console.log(sum(5));
-console.log(sum(20));
+const str =
+  "The dates between 1976-03-12, and 1983-10-14, the code was 2004-67-12 and possible invalid date 8765-11-34";
+console.log(findDates(str));
