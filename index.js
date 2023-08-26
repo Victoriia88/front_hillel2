5e73f6323f5a11d1eedc3678c8d3662a4a367736
Vulynets88 <vulynets528@gmail.com>
2023-08-26 09:11:58 +0300

    homework24

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-function findDates(input) {
-  const dateRegex = /\b(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\b/g;
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+const textField = document.getElementById("textField");
+const sideDiv = document.getElementById("sideDiv");
 
-  const matches = input.match(dateRegex);
-  if (!matches) return [];
+textField.addEventListener("focus", () => {
+  sideDiv.style.display = "block";
+});
 
-  const result = matches.map((match) => {
-    const [year, month, day] = match.split("-").map(Number);
-    return {
-      day,
-      month,
-      monthName: months[month - 1],
-      year,
-    };
-  });
-
-  return result;
-}
-
-const str =
-  "The dates between 1976-03-12, and 1983-10-14, the code was 2004-67-12 and possible invalid date 8765-11-34";
-console.log(findDates(str));
+textField.addEventListener("blur", () => {
+  sideDiv.style.display = "none";
+});
