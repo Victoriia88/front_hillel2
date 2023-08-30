de23f3b978cad2d12481c141384517135ae31912
Vulynets88 <vulynets528@gmail.com>
2023-08-30 16:45:52 +0300

    homework26

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const textField = document.getElementById("textField");
-const sideDiv = document.getElementById("sideDiv");
+document.getElementById("generateTable").addEventListener("click", function () {
+  let table = document.getElementById("numberTable");
 
-textField.addEventListener("focus", () => {
-  sideDiv.style.display = "block";
-});
+  table.innerHTML = "";
 
-textField.addEventListener("blur", () => {
-  sideDiv.style.display = "none";
+  let num = 1;
+  for (let i = 0; i < 10; i++) {
+    let row = table.insertRow(i);
+    for (let j = 0; j < 10; j++) {
+      let cell = row.insertCell(j);
+      cell.innerHTML = num++;
+    }
+  }
 });
